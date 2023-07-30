c793ede25fe696d99b31079a959af52ed1506d32
Christian Turner <ch.turner94@gmail.com>
2023-07-30 06:46:34 -0400

    working on aws implementation working on config

---
+++ src/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/chturner94/gopherBuilder/src/config"
+	//	"log"
+	//	"encoding/json"
+)
+
+func main() {
+	fmt.Println("Welecome to Gopher Builder!")
+	
+}
